e088a90206aa94fcae3510a594d69749c5d9b6a2
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-03-08 19:36:11 +0530

    .

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app=express();
 const mongoose=require('mongoose');
 const evs=require('./Schema/EvSchema');
 const path=require('path');
+const methodOverride=require('method-override');
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 mongoose.connect('mongodb://127.0.0.1:27017/EVApp').then(()=>{
     console.log("db connected");
@@ -36,15 +38,41 @@ app.get('/',async(req,res)=>{
     
 })
 
-app.get('/:EVid',async(req,res)=>{
+app.get('/show/:EVid',async(req,res)=>{
     const {EVid}=req.params;
     const ev=await evs.findById(EVid);
     res.render('show',{ev});
 })
 
-app.get('/newabc',(req,res)=>{
-    res.render('index');
+app.get('/new',(req,res)=>{
+    res.render('new');
 })
+
+app.post('/',async(req,res)=>{
+    const {name,image,pricing,address}=req.body;
+    await evs.create({name,image,pricing,address});
+    res.redirect('/');
+})
+
+app.get('/:EVid/edit',async(req,res)=>{
+    const {EVid}=req.params;
+    const data=await evs.findById(EVid);
+    res.render('edit',{data});
+})
+
+app.patch('/:EVid',async(req,res)=>{
+    const {EVid}=req.params;
+    const {name,image,pricing,address}=req.body;
+    await evs.findByIdAndUpdate(EVid,{name,image,pricing,address});
+    res.redirect(`/${EVid}`);
+})
+
+app.delete('/:EVid',async(req,res)=>{
+    const {EVid}=req.params;
+    await evs.findByIdAndDelete(EVid);
+    res.redirect('/');
+})
+
 
 app.listen(3000,()=>{
     console.log("server running");
